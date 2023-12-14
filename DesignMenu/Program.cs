1a8da60f6ffb22cb706f0ddd2745204fe66f86ca
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-14 10:07:25 +0700

    da xong

---
+++ DesignMenu/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Choise = -1;
+            while (Choise!=0)
+            {
+                Console.WriteLine("Nhap 1 ky tu tu ban phim");
+                Choise = int.Parse(Console.ReadLine());
+                switch (Choise)
+                {
+                    case 1: 
+                        Console.WriteLine("*********");
+                        Console.WriteLine("********");
+                        Console.WriteLine("*******");
+                        Console.WriteLine("******");
+                        Console.WriteLine("*****");
+                        Console.WriteLine("****");
+                        Console.WriteLine("***");
+                        Console.WriteLine("**");
+                        Console.WriteLine("*");
+                        break;
+                    case 2:
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        break;
+                    case 3:
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        break;
+                        default: Console.WriteLine("nhap lai");
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
